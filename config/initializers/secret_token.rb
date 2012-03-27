567c64be550bbd7a33f217e90cf8b14087188050
Abhilash A K <abhilash@presciencesoft.com>
2012-03-27 17:41:43 +0530

    hr software skeleton - First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HrMan::Application.config.secret_token = 'e260fb126af491a93fdf379118c957fd74cbcde138eeec7c580e7a421d9af1b7426844488de0cbbc8082876aae39b63cc698182bdef684c51e9e8f781124e2b9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+HrMan::Application.config.session_store :cookie_store, key: '_hr_man_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# HrMan::Application.config.session_store :active_record_store
